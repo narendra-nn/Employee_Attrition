65cdcab0eeb4008312b126bad3b5b5bb7bbd10d0
SAI NARENDRA <54445098+narendra-nn@users.noreply.github.com>
2022-02-20 14:55:37 +0530

    added file

---
+++ Preprocessing/preprocessing.py
@@ -0,0 +1,104 @@
+import numpy as np
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+from imblearn.over_sampling import RandomOverSampler
+
+
+class Preprocessor:
+    """
+    This Class is used to Perform Preprocessing steps which are needed to perform
+    before training the model with the data
+
+    Written by Sai Narendra
+    """
+    def __init__(self, file_object, logger_object):
+        self.file_object = file_object
+        self.logger_object = logger_object
+
+    def Encoding_columns(self,data):
+         """
+         Method Name: Encoding_columns
+         Description: This method is used to Convert Categorical features to the Encoded Version of those features
+         :param data: Input DataFrame for the process
+         :return: Returns a DataFrame with features converted from Categorical to Encoded featurws
+         On Failure: Raises an Exception
+         """
+
+
+
+
+
+
+
+    def removing_columns(self,data,columns):
+        """
+        Method Name: removing_columns
+        Description: This method is used to Remove the features in the training data which are not required for training the model
+        param data:  Training Data
+        param columns: Input which contains the features needed to be removed
+        :return: Returns a Dataset with necessary columns for training
+        On Failure: Raises an Exception
+        """
+        self.logger_object.log(self.file_object, 'Entered the removing_columns method of the Preprocessor class')
+        self.data = data
+        self.columns = columns
+        try:
+            self.useful_data = self.data.drop(self.columns, axis=1)  # drop the labels specified in the columns
+            self.logger_object.log(self.file_object,'Column removal Successful.Exited the remove_columns method of the Preprocessor class')
+            return self.useful_data
+        except Exception as e:
+            self.logger_object.log(self.file_object,
+                                   'Exception occured in remove_columns method of the Preprocessor class. Exception message:  ' + str(e))
+            self.logger_object.log(self.file_object,'Column removal Unsuccessful. Exited the remove_columns method of the Preprocessor class')
+            raise Exception()
+
+    def separate_label_feature(self, data, label_column_name):
+        """
+                        Method Name: separate_label_feature
+                        Description: This method separates the features and a Label Coulmns.
+                        Output: Returns two separate Dataframes, one containing features and the other containing Labels .
+                        On Failure: Raise Exception
+
+                """
+        self.logger_object.log(self.file_object, 'Entered the separate_label_feature method of the Preprocessor class')
+        try:
+            self.X=data.drop(labels=label_column_name,axis=1) # drop the columns specified and separate the feature columns
+            self.Y=data[label_column_name] # Filter the Label columns
+            self.logger_object.log(self.file_object,
+                                   'Label Separation Successful. Exited the separate_label_feature method of the Preprocessor class')
+            return self.X,self.Y
+        except Exception as e:
+            self.logger_object.log(self.file_object,'Exception occured in separate_label_feature method of the Preprocessor class. Exception message:  ' + str(e))
+            self.logger_object.log(self.file_object, 'Label Separation Unsuccessful. Exited the separate_label_feature method of the Preprocessor class')
+            raise Exception()
+
+
+
+    def scale_numerical_columns(self,data):
+        """
+        Method Name: scale_numerical_columns
+        Description: This method scales the numerical values using the Standard scaler.
+        Output: A dataframe with scaled values
+        On Failure: Raise Exception
+        """
+
+        self.logger_object.log(self.file_object,
+                               'Entered the scale_numerical_columns method of the Preprocessor class')
+
+        self.data=data
+        #self.num_df = self.data.drop(["potential_issue","deck_risk","ppap_risk","stop_auto_buy","rev_stop"],axis=1)
+
+        try:
+
+            self.scaler = StandardScaler()
+            self.scaled_data = self.scaler.fit_transform(self.num_df)
+            self.scaled_num_df = pd.DataFrame(data=self.scaled_data, columns=self.num_df.columns,index=self.data.index)
+            self.data.drop(columns=self.scaled_num_df.columns, inplace=True)
+            self.data = pd.concat([self.scaled_num_df, self.data], axis=1)
+
+            self.logger_object.log(self.file_object, 'scaling for numerical values successful. Exited the scale_numerical_columns method of the Preprocessor class')
+            return self.data
+        except Exception as e:
+            self.logger_object.log(self.file_object,'Exception occured in scale_numerical_columns method of the Preprocessor class. Exception message:  ' + str(e))
+            self.logger_object.log(self.file_object, 'scaling for numerical columns Failed. Exited the scale_numerical_columns method of the Preprocessor class')
+            raise Exception()
